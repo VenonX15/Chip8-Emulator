ad114248775b4afecc1d0efeba59d1a32cdf082e
VenonX15 <venonx15@gmail.com>
2026-05-10 19:25:58 +0200

    Delete Chip8_Emulator directory

--- Chip8_Emulator/chip8/debug/breakpoints.py
+++
@@ -1,9 +0,0 @@
-class Breakpoints:
-    def __init__(self):
-        self.points = set()
-
-    def add(self, addr):
-        self.points.add(addr)
-
-    def check(self, pc):
-        return pc in self.points
